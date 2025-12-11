7f7df4aca73db2a8ef39205deb4710c3cf49fdb9
sudo-gera <gera.tatarinov.999@gmail.com>
2025-12-11 08:37:10 +0300

    *interesting commit message*

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ cd ~/a54
 while sleep 1
 do
     date
-    ifconfig | grep inet | sed -E s' \s+\S+\s+(\S+).* \1 ' | sort | uniq | grep -v 127.0.0.1 > ip.txt
+    ifconfig | grep inet | sed -E s' \s+\S+\s+(\S+).* \1 ' | sort | uniq | grep -v 127.0.0.1 | tee /dev/stderr > ip.txt
     if [ -n "$(git status --porcelain)" ]
     then
         ~/c/gitpush
