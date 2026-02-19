408bd1ec189210d3122eb415b7d855dd67c40818
sudo-gera <gera.tatarinov.999@gmail.com>
2026-02-19 22:56:30 +0300

    *interesting commit message*

--- update.sh
+++ update.sh
@@ -8,11 +8,7 @@ while sleep 1
 do
     date
     ifconfig | grep inet | sed -E s' \s+\S+\s+(\S+).* \1 ' | sort | uniq | grep -v 127.0.0.1 | tee /dev/stderr > ip.txt
-#    for i in $(seq 1 256)
-#    do
-#        cp ip.txt ip${i}.txt
-#    done
-    if [ -n "$(git status --porcelain)" ]
+    if [ -n "$(git diff origin/main)" ]
     then
         ~/c/gitpush
     fi
